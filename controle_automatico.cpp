c5ab3ad6418e46a62d4bc692fb8c544e021b76c1
viniciusvidal2 <vinicius.vidal@engenharia.ufjf.br>
2018-01-04 11:09:26 -0200

    Primeira versao funcionando motores, logica a testar

--- controle_automatico.cpp
+++ controle_automatico.cpp
@@ -10,6 +10,10 @@
 #include <mavros_msgs/mavlink_convert.h>
 #include <mavros_msgs/VFR_HUD.h>
 
+#include <dynamixel_workbench_toolbox/dynamixel_multi_driver.h>
+#include <dynamixel_workbench_msgs/JointCommand.h>
+#include <dynamixel_workbench_msgs/DynamixelStateList.h>
+
 class PixhawkeMotor
 {
 private:
@@ -21,6 +25,8 @@ private:
     float ang_yaw_range[2]   = {0.0  , 300.0}; // [DEGREES]
     float ang_pitch_range[2] = {145.0, 214.0}; // [DEGREES]
     float ang_pitch_horizontal = 173.0; // [DEGREES] 1976 DE PWM
+    int pwm_pitch_horizontal = 1976; // [PWM]
+    int pwm_yaw_frente = 512; // apontar sempre para frente do veiculo caso nao precise virar [PWM]
     float yaw_mid_range, pitch_mid_range; // [DEGREES]
     // Relacao pwm/ang[DEGREES] para os dois casos
     float pwm_ang_yaw, pwm_ang_pitch;
@@ -33,6 +39,8 @@ private:
     ros::NodeHandle nh_;
     ros::Subscriber subPix;
     ros::ServiceClient motor;
+    // Mensagem destinada ao servico do motor
+    dynamixel_workbench_msgs::JointCommand posicao;
 
 public:
     PixhawkeMotor()
@@ -43,13 +51,16 @@ public:
         pwm_ang_yaw   = (pwm_yaw_range[1] - pwm_yaw_range[0]) / (ang_yaw_range[1] - ang_yaw_range[0]);         // [PWM/DEGREES]
         pwm_ang_pitch = (pwm_pitch_range[1] - pwm_pitch_range[0]) / (ang_pitch_range[1] - ang_pitch_range[0]); // [PWM/DEGREES]
         // Pontos centrais dos dois servos
-        yaw_mid_range   = (ang_yaw_range[1]   - ang_yaw_range[0])  /2; // [DEGREES]
-        pitch_mid_range = (ang_pitch_range[1] - ang_pitch_range[0])/2; // [DEGREES]
+        yaw_mid_range   = (ang_yaw_range[1]   + ang_yaw_range[0])  /2; // [DEGREES]
+        pitch_mid_range = (ang_pitch_range[1] + ang_pitch_range[0])/2; // [DEGREES]
+        // Servico que envia valores ao motor
+        motor = nh_.serviceClient<dynamixel_workbench_msgs::JointCommand>("/joint_command");
     }
 
     int ExecutarClasse(int argc, char **argv)
     {
         calcularAngulosMotores();
+        enviarAngulosMotores();
     }
 
 private:
@@ -57,18 +68,31 @@ private:
     {
       // Analisando diferenca de yaw
       delta_yaw = wrap180(yaw_atual, yaw_para_apontar);
+      ROS_INFO("delta yaw: %.2f", yaw_mid_range);
       ang_pan   = ((yaw_mid_range + delta_yaw) < ang_yaw_range[1]) ? yaw_mid_range + delta_yaw : ang_yaw_range[1]; // LImitando maximo
       ang_pan   = (ang_pan > ang_yaw_range[0]) ? ang_pan : ang_yaw_range[0]; // LImitando minimo
 
-      // Analisando diferenca de pitch
-      delta_pitch = pitch_para_apontar; // TODO: verificar sentido de rotacao -> sinal
-      ang_tilt    = ((ang_pitch_horizontal + delta_pitch) > ang_pitch_range[1]) ? ang_pitch_horizontal + delta_pitch : ang_pitch_range[1];
-      ang_tilt    = ang_tilt > ang_pitch_range[0] ? ang_tilt : ang_pitch_range[0];
+      // Analisando diferenca de pitch -> somente a mesma sobre o pwm para manter horizontal
+      delta_pitch = ((ang_pitch_horizontal + pitch_para_apontar) < ang_pitch_range[1]) ? pitch_para_apontar : ang_pitch_range[1] - ang_pitch_horizontal;
 
-      // Uma vez todos os angulos calculados, converter para valor de pwm
-      pwm_pan  = pwm_yaw_range[0]   + (ang_pan-ang_yaw_range[0])   *pwm_ang_yaw;
-      pwm_tilt = pwm_pitch_range[0] + (ang_tilt-ang_pitch_range[0])*pwm_ang_pitch; // Aqui ja pode enviar aos motores na funcao principal
+      // Uma vez todos os angulos calculados, converter para valor de pwm para enviar aos motores
+      pwm_pan  = pwm_yaw_range[0] + (ang_pan - ang_yaw_range[0])*pwm_ang_yaw;
+      pwm_tilt = pwm_pitch_horizontal + delta_pitch*pwm_ang_pitch;
+    }
 
+    void enviarAngulosMotores()
+    {
+      ROS_INFO("Angulo PAN : %d", pwm_pan);
+      ROS_INFO("Angulo TILT: %d", pwm_tilt);
+
+      posicao.request.pan_pos  = (int)pwm_pan;
+      posicao.request.tilt_pos = (int)pwm_tilt;
+      posicao.request.unit     = "raw";
+
+      posicao.response.pan_pos = (int)pwm_pan;
+      posicao.response.tilt_pos = (int)pwm_tilt;
+
+      motor.call(posicao);
     }
 
     float rad2deg(float rad)
@@ -80,8 +104,8 @@ private:
     float wrap180(float atual, float apontar)
     {
         float delta = atual - apontar;
-        if(delta >  180) {delta -= 360.0;}
-        if(delta < -180) {delta += 360.0;}
+        if(delta >  180.0) {delta -= 360.0;}
+        if(delta < -180.0) {delta += 360.0;}
 
         return delta;
     }
@@ -95,9 +119,9 @@ private:
         yaw_para_apontar   = ((float)(msg->heading)*0.01 >= 0) ? (float)(msg->heading)*0.01 : yaw_atual; // [DEGREES]
 //        yaw_para_apontar = msg->heading*0.01;
         // Mostrando na tela se esta tudo ok
-        ROS_INFO("Pitch: [%.2f]", pitch_para_apontar);
-        ROS_INFO("Yaw:   [%.2f]", yaw_atual);
-        ROS_INFO("ALvo:  [%.2f]", yaw_para_apontar);
+//        ROS_INFO("Pitch: [%.2f]", pitch_para_apontar);
+//        ROS_INFO("Yaw:   [%.2f]", yaw_atual);
+//        ROS_INFO("ALvo:  [%.2f]", yaw_para_apontar);
     }
 
 };
